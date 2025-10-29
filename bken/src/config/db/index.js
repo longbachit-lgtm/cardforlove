cbcab4050248e8b597883b8ac0b0a1751e772a0d
longbachit-lgtm <longbach.it@gmail.com>
2025-10-29 10:21:30 +0700

    flash mongo

--- bken/src/config/db/index.js
+++ bken/src/config/db/index.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
-
+// mongodb://localhost:27017/card_love
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/card_love', {
-        
+
+        const USER = 'longbachit_db_user';
+        const PASS = 'abcd@1234';                 // có ký tự @
+        const HOST = 'cluster0.rfu7diy.mongodb.net';
+
+        const uri = `mongodb+srv://${encodeURIComponent(USER)}:${encodeURIComponent(PASS)}@${HOST}/?appName=Cluster0`;
+        await mongoose.connect(uri, {
+
             useUnifiedTopology: true,
-  
+
         });
         console.log('Connect successfully!!!');
     } catch (error) {
